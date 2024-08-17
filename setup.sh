c78701df8aa7ee9804ccb5ea8205172aa526445e
ASanchz85 <asanchz85@gmail.com>
2024-08-17 17:46:07 +0200

    FEAT: updating folder structure and eslint plugins version

--- setup.sh
+++ setup.sh
@@ -10,18 +10,11 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-# Create mail folder
-mkdir "$1"
-
-# Create subfolders
-mkdir "$1/routes"
-mkdir "$1/controllers"
-mkdir "$1/services"
-mkdir "$1/utils"
-mkdir "$1/public"
+# Create main folder and subfolders
+mkdir -p "$1/src/config" "$1/src/models" "$1/src/routes" "$1/src/controllers" "$1/src/tests" "$1/src/middlewares" "$1/src/services" "$1/src/utils" "$1/public"
 
 # Create index.js file
-touch "$1/index.js"
+touch "$1/index.js" "$1/src/config/constants.js" "$1/src/config/db.js" "$1/src/server.js" "$1/src/routes/.gitkeep" "$1/src/controllers/.gitkeep" "$1/src/middlewares/.gitkeep" "$1/src/services/.gitkeep" "$1/src/utils/.gitkeep" "$1/src/utils/logger.js" "$1/src/utils/response.js" "$1/src/utils/validation.js"
 
 # Change directory to the custom folder
 cd "$1" || exit
@@ -31,42 +24,24 @@ npm init -y
 git init
 
 # Create .env file
-echo "BASE_URL=http://localhost\nPORT=3000" > .env
+echo -e "BASE_URL=http://localhost\nPORT=3000" > .env
 
-# Navigate to the directory where the script resides
-cd "$script_dir" || exit
-
-# Search for the template_gitignore file recursively starting from the current directory
-template_gitignore_path=$(find . -type f -name "template_gitignore" -print -quit)
-
-# Check if the template_gitignore file was found
-if [ -n "$template_gitignore_path" ]; then
-    # Copy the template_gitignore file to the current directory
-    cd "$current_dir" || exit
-    cd "$1" || exit
-    cp "$script_dir/$template_gitignore_path" ./.gitignore
+# Check and copy .gitignore template
+if [ -f "$script_dir/template_gitignore" ]; then
+  cp "$script_dir/template_gitignore" ./.gitignore
 else
-    echo "Error: template_gitignore file not found."
+  echo "Error: template_gitignore file not found."
 fi
 
-# Navigate to the directory where the script resides
-cd "$(dirname "$0")" || exit
-
-# Search for the template_package.json file recursively starting from the current directory
-template_package_json_path=$(find . -type f -name "template_package.json" -print -quit)
-
-# Check if the template_package.json file was found
-if [ -n "$template_package_json_path" ]; then
-    # Copy the template_package.json file to the current directory
-    cd "$current_dir" || exit
-    cd "$1" || exit
-    cp "$script_dir/$template_package_json_path" ./package.json
+# Check and copy package.json template
+if [ -f "$script_dir/template_package.json" ]; then
+  cp "$script_dir/template_package.json" ./package.json
 else
-    echo "Error: template_package.json file not found."
+  echo "Error: template_package.json file not found."
 fi
 
 # Update package.json with the new name
-sed -i 's/"name": "server"/"name": "@server\/'"$1"'"/' package.json
+sed -i "s/\"name\": \"server\"/\"name\": \"server-$1\"/" package.json
 
 # Install dependencies
 npm i -E express dotenv cors helmet jsonwebtoken bcryptjs cookie-parser body-parser
@@ -75,19 +50,13 @@ npm i -DE nodemon morgan standard
 # Prompt for installing eslint
 read -r -p "Do you want to install eslint? (y/n): " install_eslint
 if [[ $install_eslint =~ ^[Yy]$ ]]; then
-  npm i -DE eslint eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-standard
+  npm install -DE eslint@8.57.0 eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-promise@6.6.0 eslint-plugin-unused-imports eslint-plugin-jsdoc
 
-  # Search for the template_eslintrc.json file recursively starting from the current directory
-  template_eslintrc_json_path=$(find . -type f -name "template_eslintrc.json" -print -quit)
-
-  # Check if the template_eslintrc.json file was found
-  if [ -n "$template_eslintrc_json_path" ]; then
-      # Copy the template_eslintrc.json file to the current directory
-      cd "$current_dir" || exit
-      cd "$1" || exit
-      cp "$script_dir/$template_eslintrc_json_path" ./.eslintrc.json
+  # Check and copy .eslintrc.json template
+  if [ -f "$script_dir/template_eslintrc.json" ]; then
+    cp "$script_dir/template_eslintrc.json" ./.eslintrc.json
   else
-      echo "Error: template_eslintrc.json file not found."
+    echo "Error: template_eslintrc.json file not found."
   fi
 fi
 
@@ -95,26 +64,18 @@ fi
 read -r -p "Do you want to install swagger? (y/n): " install_swagger
 if [[ $install_swagger =~ ^[Yy]$ ]]; then
   npm i -E swagger-jsdoc swagger-ui-express
-  mkdir config
-  mkdir models
-  touch config/swagger.js
+  touch src/config/swagger.js
 fi
 
 # Prompt for installing mongoose
 read -r -p "Do you want to install mongoose? (y/n): " install_mongoose
 if [[ $install_mongoose =~ ^[Yy]$ ]]; then
   npm i -E mongoose
-  mkdir config
-  mkdir models
-fi
-
-# Prompt for installing prisma if mongoose is not installed
-if [ ! -z "$install_mongoose" ] && [[ ! $install_mongoose =~ ^[Yy]$ ]]; then
+else
+  # Prompt for installing prisma if mongoose is not installed
   read -r -p "Do you want to install prisma? (y/n): " install_prisma
   if [[ $install_prisma =~ ^[Yy]$ ]]; then
     npm i -E prisma
-  mkdir config
-  mkdir models
   fi
 fi
 
@@ -138,8 +99,8 @@ case "$module_choice" in
 esac
 
 # Creating a simple README.md file
-echo '# '"$1" > README.md
-echo 'type *npm run dev* for starting the developing mode' >> README.md
+echo "# $1" > README.md
+echo "type *npm run dev* for starting the developing mode" >> README.md
 
 # Display success message
 echo "Setup completed successfully!"
